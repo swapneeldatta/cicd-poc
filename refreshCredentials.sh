83a98f96c22e22189e992cf5e376a50b21845e2c
Swapneel Datta <swapneeldatta@dattus.com>
2018-05-14 18:10:47 +0530

    30

--- refreshCredentials.sh
+++ refreshCredentials.sh
@@ -2,10 +2,10 @@
 
 # Enter your Ubuntu One e-mail address and password. Required for Launchpad to
 # connect to git 4 times a day to check for updates.
-snapcraft export-login creds
+snapcraft export-login credentials
 
 # Encrypt the credential files
-openssl aes-256-cbc -e -in creds -out .circleci/credentials.enc
+openssl des-ede3-ofb -e -in credentials -out .circleci/credentials.enc
 
 # Cleanup!
-rm creds
+#rm creds
