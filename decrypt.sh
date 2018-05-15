d757a248f9611c16dca704d030e3505d3d566bc7
Anupama Khangar <akhangar@dattus.com>
2018-05-15 11:34:39 +0530

    z

--- decrypt.sh
+++ decrypt.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 SNAPCRAFT_CREDENTIALS_KEY=password
-openssl -aes-256-cfb8 -d -bufsize 8 -in .circleci/credentials.enc -out credentials -k $SNAPCRAFT_CREDENTIALS_KEY -md sha1
+openssl enc -aes-256-cfb8 -d -bufsize 8 -in .circleci/credentials.enc -out credentials -k $SNAPCRAFT_CREDENTIALS_KEY -md sha1
 
